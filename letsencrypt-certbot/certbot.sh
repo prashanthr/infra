b546d135e6a87ce02b5694e83db598bbfcecae80
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-06-30 18:25:24 -0700

    final fixes

--- letsencrypt-certbot/certbot.sh
+++ letsencrypt-certbot/certbot.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-certbot --authenticator webroot --installer nginx -n --agree-tos --redirect --staging -d comical.site --email $EMAIL --webroot-path $WEB_ROOT_PATH
+certbot --authenticator webroot --installer nginx -n --agree-tos --redirect --staging -d $DOMAIN --email $EMAIL --webroot-path $WEB_ROOT_PATH
