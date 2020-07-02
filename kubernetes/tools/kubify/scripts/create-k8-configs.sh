9400132aa0cd2e5f13c7f499c8fec2c8d4849241
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-07-01 17:52:29 -0700

    script fully functional

--- kubernetes/tools/kubify/scripts/create-k8-configs.sh
+++ kubernetes/tools/kubify/scripts/create-k8-configs.sh
@@ -1,4 +1,7 @@
 #/bin/bash
+# Example usage
+# K8_APP_NAMESPACE=namespace K8_APP_NAME=test K8_APP_PORT=7000 K8_APP_IMAGE="hello\/hello" ./scripts/create-k8-configs.sh
+
 function replaceOccurence {
     echo "Replacing $1 for $2 in $3"
     local search=$1
@@ -22,7 +25,7 @@ for directory in $(find $TARGET_DIR -type d);
         if [ -f "$file" ]; then
           replaceOccurence __K8_APP_NAMESPACE__ $K8_APP_NAMESPACE $file
           replaceOccurence __K8_APP_NAME__ $K8_APP_NAME $file
-          replaceOccurence __K8_APP_IMAGE__ "$K8_APP_IMAGE" $file
+          replaceOccurence __K8_APP_IMAGE__ $K8_APP_IMAGE $file
           replaceOccurence __K8_APP_PORT__ $K8_APP_PORT $file
         fi
       done
