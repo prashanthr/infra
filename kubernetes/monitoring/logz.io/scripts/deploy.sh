dd59b90c374cb56860aff024af21ffe8e9f59c60
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-07-06 15:36:11 -0700

    feat(logs): Add logz.io monitoring

---
+++ kubernetes/monitoring/logz.io/scripts/deploy.sh
@@ -0,0 +1,11 @@
+# https://app.logz.io/#/dashboard/data-sources/Kubernetes
+if [ -z "$LOGZIO_TOKEN" ]; then
+  echo "No token provided. Exiting..."
+  exit 1
+fi
+echo "Found token"
+kubectl create secret generic logzio-logs-secret --from-literal=logzio-log-shipping-token=$LOGZIO_TOKEN --from-literal=logzio-log-listener='https://listener.logz.io:8071' -n kube-system
+# RBAC
+kubectl apply -f https://raw.githubusercontent.com/logzio/logzio-k8s/master/logzio-daemonset-rbac.yaml
+# Non RBAC
+#kubectl apply -f https://raw.githubusercontent.com/logzio/logzio-k8s/master/logzio-daemonset.yaml
