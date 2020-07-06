14ff4237159bd8e69bf7ef9a8d68ae46f3a54fc5
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-07-06 15:55:32 -0700

    add more comments

--- kubernetes/monitoring/logz.io/scripts/deploy.sh
+++ kubernetes/monitoring/logz.io/scripts/deploy.sh
@@ -4,6 +4,8 @@ if [ -z "$LOGZIO_TOKEN" ]; then
   exit 1
 fi
 echo "Found token"
+# The below host and port depends on listener region
+# https://docs.logz.io/shipping/log-sources/elastic-kubernetes-service.html
 kubectl create secret generic logzio-logs-secret --from-literal=logzio-log-shipping-token=$LOGZIO_TOKEN --from-literal=logzio-log-listener='https://listener.logz.io:8071' -n kube-system
 # RBAC
 kubectl apply -f https://raw.githubusercontent.com/logzio/logzio-k8s/master/logzio-daemonset-rbac.yaml
