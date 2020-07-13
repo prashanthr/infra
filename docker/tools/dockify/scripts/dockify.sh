1ba0217172852bf41f2e29712167fba04373bd54
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2020-07-13 14:15:43 -0700

    chore(docker): Make dockify more extensible (#5)
    Co-authored-by: Prashanth R <1409501+prashanthr@users.noreply.github.com>

--- docker/tools/dockify/scripts/dockify.sh
+++ docker/tools/dockify/scripts/dockify.sh
@@ -18,6 +18,15 @@
 # DOCKER_PORT=9000 \
 # ./scripts/dockify.sh
 
+# APP_LANGUAGE="node" \
+# APP_TYPE="backend" \
+# DOCKER_IMAGE_VERSION=latest \
+# DOCKER_IMAGE_NAME=test \
+# DOCKER_IMAGE_DESCRIPTION="test description" \
+# DOCKER_IMAGE_MAINTAINER="author \<http\:\/\/github.com/author\>" \
+# DOCKER_PORT=9000 \
+# ./scripts/dockify.sh
+
 function replaceOccurence {
     echo "Replacing $1 for $2 in $3"
     local search=$1
@@ -27,8 +36,17 @@ function replaceOccurence {
 }
 
 # Copy template
-echo "Copying from template to output dir..."
-cp -r ./template/ ./output/
+if [ -z $LANGUAGE ]; then
+  APP_LANGUAGE="node"
+fi
+
+if [ -z $APP_TYPE ]; then
+ APP_TYPE="fullstack"
+fi
+
+SOURCE_PATH=./template/$APP_LANGUAGE/$APP_TYPE/
+echo "Copying from $SOURCE_PATH to output dir..."
+cp -r $SOURCE_PATH ./output/
 
 # Replace occurences for files in subfolders
 TARGET_DIR=./output
