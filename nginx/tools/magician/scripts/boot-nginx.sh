d290dee6305750c6bffe4528b22e7d4b47b8d797
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-10-30 16:35:58 -0700

    change alias to fn

--- nginx/tools/magician/scripts/boot-nginx.sh
+++ nginx/tools/magician/scripts/boot-nginx.sh
@@ -1,7 +1,13 @@
 #/bin/bash
-alias NGINX_STATUS='sudo service nginx status'
-alias NGINX_RELOAD='sudo service nginx reload'
-alias NGINX_RESTART='sudo service nginx restart'
+function NGINX_STATUS {
+  sudo service nginx status
+}
+function NGINX_RELOAD {
+  sudo service nginx reload
+}
+function NGINX_RESTART {
+  sudo service nginx restart
+}
 
 NGINX_STATUS
 NGINX_RELOAD
