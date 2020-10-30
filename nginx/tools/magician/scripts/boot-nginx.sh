c0f9c3dc5c6175cd346a945d013e6c7b01a85e45
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-10-30 16:39:42 -0700

    revert boot nginx

--- nginx/tools/magician/scripts/boot-nginx.sh
+++ nginx/tools/magician/scripts/boot-nginx.sh
@@ -1,14 +1,4 @@
 #/bin/bash
-function NGINX_STATUS {
-  sudo service nginx status
-}
-function NGINX_RELOAD {
-  sudo service nginx reload
-}
-function NGINX_RESTART {
-  sudo service nginx restart
-}
-
-NGINX_STATUS
-NGINX_RELOAD
-NGINX_RESTART
+sudo service nginx status
+sudo service nginx reload
+sudo service nginx restart
