36b5490cc66c2810577214b11402556615c5c068
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-11-02 17:02:23 -0800

    fix symlink basename

--- nginx/tools/magician/scripts/copy-app-configs.sh
+++ nginx/tools/magician/scripts/copy-app-configs.sh
@@ -6,7 +6,8 @@ SITES_AVAILABLE_PATH=$NGINX_PATH/sites-available
 SITES_ENABLED_PATH=$NGINX_PATH/sites-enabled
 echo "Copying app NGINX configs to $SITES_AVAILABLE_PATH..."
 cp -r $CFG_PATH/*.lua $SITES_AVAILABLE_PATH
-for cfgFile in $CFG_PATH/*.lua; do
+for file in $CFG_PATH/*.lua; do
+  cfgFile=$(basename $file)
   echo "Creating symlink for $SITES_AVAILABLE_PATH/$cfgFile at $SITES_ENABLED_PATH/$cfgFile..."
   sudo ln -s $SITES_AVAILABLE_PATH/$cfgFile $SITES_ENABLED_PATH/$cfgFile
 done
